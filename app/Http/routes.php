605e1b4971a73829e2b5a41da9d53210c4bd2934
Damian Ippolito <dippolito@dimaticasoftware.com>
2017-09-08 14:56:50 +0200

    Section 5 Video 29

--- app/Http/routes.php
+++ app/Http/routes.php
@@ -11,6 +11,10 @@
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
-});
+//Route::get('/', function () {
+//    return view('welcome');
+//});
+
+//Route::get('/post/{id}', 'PostController@index' );
+
+Route::resource('posts', 'PostController');
